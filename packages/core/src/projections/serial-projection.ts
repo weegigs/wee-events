2d463656567e9ec7fe18a167cd03914be35c174e
Kevin O'Neill <kevin@oneill.id.au>
2017-12-12 14:14:58 +1100

    feat(core): Serialize now manages one worker queue per aggregate id

--- packages/core/src/projections/serial-projection.ts
+++ packages/core/src/projections/serial-projection.ts
@@ -1,27 +1,43 @@
-import { queue, AsyncWorker } from "async";
+import { queue, AsyncWorker, AsyncQueue } from "async";
+import { Map } from "immutable";
 
 import { ProjectionFunction } from "./types";
-import { PublishedEvent } from "../index";
+import { PublishedEvent, AggregateId } from "../index";
 import { config } from "../config";
 
 export function serialize<T>(projection: ProjectionFunction<T>): ProjectionFunction<T> {
-  const worker: AsyncWorker<PublishedEvent<T>, Error> = async (event, done) => {
-    try {
-      await projection(event);
-    } catch (error) {
-      done(error);
-    }
+  let queues: Map<string, AsyncQueue<PublishedEvent<T>>> = Map();
+
+  const worker: AsyncWorker<PublishedEvent<T>, Error> = async event => {
+    await projection(event);
   };
 
-  const jobs = queue(worker, 1);
+  const queueForAggregate = (aggregateId: AggregateId): AsyncQueue<PublishedEvent<T>> => {
+    const { type, id } = aggregateId;
+    const key = `${type}|${id}`;
+    let q = queues.get(key);
+    if (q === undefined) {
+      q = queue(worker, 1);
+      q.drain = () => {
+        queues = queues.delete(key);
+        config.logger.debug(`projection queue removed`, { type, id });
+      };
+      queues = queues.set(key, q);
+      config.logger.debug(`projection queue added`, { type, id });
+    }
 
-  return (event: PublishedEvent<T>) =>
+    return q;
+  };
+
+  return (event: PublishedEvent<T>) => {
+    const jobs = queueForAggregate(event.aggregateId);
     jobs.push(event, (error: any) => {
       if (error) {
         config.logger.warn("projection failed to process event", {
           event: event.id,
-          error: error.message || error,
+          error,
         });
       }
     });
+  };
 }
