fb90fa546e8d96398dc3fd506e78f21f8a2823cc
Kevin O'Neill <kevin@oneill.id.au>
2017-12-16 14:05:29 +1100

    fix(mongodb): Fix return type of DocumentProjectionFunction

--- packages/mongodb/src/mongodb/types.ts
+++ packages/mongodb/src/mongodb/types.ts
@@ -1,12 +1,15 @@
 import { PublishedEvent, EventStreamOptions } from "@weegigs/events-core";
 
-export type DocumentProjectionFunction<T> = (event: PublishedEvent, current?: T) => Promise<T | undefined>;
+export type DocumentProjectionFunction<T, R = any> = (
+  event: PublishedEvent,
+  current?: T
+) => Promise<R | undefined>;
 
-export interface DocumentProjectionOptions<T> extends EventStreamOptions {
+export interface DocumentProjectionOptions<T, R = any> extends EventStreamOptions {
   /** Name of the projection */
   name: string;
   /** projection function to run */
-  projection: DocumentProjectionFunction<T>;
+  projection: DocumentProjectionFunction<T, R>;
   /** Only process events of type. Defaults to all types */
   events?: string | string[];
   /** Provide existing data to projection function? Defaults to false */
