98814eafd1ebaa1ca21cc934eea3175bee406ea8
Kevin O'Neill <kevin@oneill.id.au>
2018-01-15 18:28:34 +1100

    feat: Representation functions

--- packages/core/src/projections/types.ts
+++ packages/core/src/projections/types.ts
@@ -1,7 +1,6 @@
 import { EventId, PublishedEvent } from "../types";
-import { AggregateId } from "../aggregate";
 
 export type ProjectionPosition = EventId | undefined;
 export type ProjectionFunction<T = any> = (event: PublishedEvent<T>) => void | Promise<void>;
 
-export type RepresentationFunction = <T>(id: AggregateId) => Promise<T>;
+export type RepresentationFunction = <T>(events: PublishedEvent[], seed?: T) => Promise<T>;
