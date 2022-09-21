740a0c3f77acf410dfa1ac6df93d630408b5f394
Kevin O’Neill <kevin@oneill.id.au>
2022-09-21 10:23:59 +1000

    test: set sepecific region in test dynamo client

--- packages/dynamo-event-store/src/store.spec.ts
+++ packages/dynamo-event-store/src/store.spec.ts
@@ -58,6 +58,7 @@ describe("dynamo store", () => {
     const dynamo = new DynamoDBClient({
       endpoint: `http://${dbc.getHost()}:${dbc.getMappedPort(8000)}`,
       credentials: { accessKeyId: "fake-key", secretAccessKey: "fake-secret" },
+      region: "us-east-1",
     });
 
     const create = new CreateTableCommand({
